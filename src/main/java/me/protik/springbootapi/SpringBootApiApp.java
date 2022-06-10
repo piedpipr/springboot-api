384db039ead5328c95d8b3fc45256f9c34996c1f
piedpipr <43669876+piedpipr@users.noreply.github.com>
2022-06-11 01:05:37 +0600

    Basic SpringBoot Application Boilerplate

---
+++ src/main/java/me/protik/springbootapi/SpringBootApiApp.java
@@ -0,0 +1,13 @@
+package me.protik.springbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApiApp.class, args);
+
+    }
+
+    }
